710acb4578b1cd46342ae42f4bc1c5cf8d3e88ae
Hill <hill@kimo.com>
2016-04-06 21:01:48 +0000

    integration with pmvc alias

--- annotation.php
+++ annotation.php
@@ -10,25 +10,41 @@ class annotation extends \PMVC\PlugIn
 {
     public function get($s)
     {
-        $doc = null;
-        $reader = new AnnotationReader(); 
-        if (is_string($s)) {
-            if (class_exists($s)) {
-                $doc = $reader->getClass($s);
-            } elseif (function_exists($s)) {
-                $doc = $reader->getFunction($s);
-            }
-        } elseif ( is_array($s) && is_callable($s) ) {
-            $doc = $reader->getMethod($s[0],$s[1]);
-        }
-        if (is_a($s,'Closure')) {
-            $doc = $reader->getFunction($s);
-        }
+        $doc = $this->getRawAnnotation($s);
         if (is_null($doc)) {
             return !trigger_error('Can\'t find annotation. '.print_r($s,true));
         }
 
         $parser = new AnnotationParser($doc);
         return $parser; 
+    }
+
+    public function getRawAnnotation($s)
+    {
+        $reader = new AnnotationReader(); 
+        if (is_string($s)) {
+            if (class_exists($s)) {
+                return $reader->getClass($s);
+            } elseif (function_exists($s)) {
+                return $reader->getFunction($s);
+            }
+        } elseif ( is_array($s) && is_callable($s) ) {
+            if (method_exists($s[0],$s[1])) {
+                return $reader->getMethod($s[0],$s[1]);
+            } elseif (is_callable([$s[0], 'isCallable'])) {
+                $func = $s[0]->isCallable($s[1]);
+                if ($func) {
+                    if (is_object($func)) {
+                        return $this->getRawAnnotation([$func,'__invoke']);
+                    } else {
+                        return $this->getRawAnnotation($func);
+                    }
+                }
+            }
+        }
+        if (is_a($s,'Closure')) {
+            return $reader->getFunction($s);
+        }
+        return null;
     }
 }
