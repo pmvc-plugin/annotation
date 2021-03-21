66282c4bf2c8b7a32e6432f9b8760a06fc67e47c
Hill <hill@kimo.com>
2021-03-21 17:25:16 +0800

    update php annotation test

--- phpunit.sh
+++ phpunit.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker-compose run --rm phpunit phpunit --no-configuration --bootstrap ./include_test.php ./tests-legacy/test.php 
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+PLUGIN_NAME="annotation"
+
+docker run --rm \
+  -v $DIR:/var/www/html \
+  -v $DIR:/var/www/${PLUGIN_NAME} \
+  --name phpunit hillliu/pmvc-phpunit:5.6 \
+  phpunit --no-configuration --bootstrap ./include_test.php ./tests-legacy/test.php
